15cf4f28e61bb9932cd64b9435ba60991d123ca0
karasicevic <ak20190190@student.fon.bg.ac.rs>
2023-05-17 17:43:00 +0200

    Zavrsena logika za pocetnu, login i register bez headera

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lokano Hall</title>
+    <link rel="stylesheet" href="CSS/design.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>    
+</head>
+<body>
+<?php  include'./header_footer/header.php';?>
+
+<div class="baner">
+  <h1><b>Lokano- Make memories with us!</b></h1>
+ <br>
+   <a href="./form.php">Log in</a> 
+</br>
+</div>
+<div class="wrapper">
+    <h1>NEWS</h1>
+    <div class="img-area">
+      <div class="single-img"><img src="images/hall2.jpg" alt=""></div>
+      <div class="single-img"><img src="images/hall1.jpg"></div>
+      <div class="single-img"><img src="images/hall3.jpg">
+       </div>
+    </div>
+    <?php include './header_footer/footer.php';?>
+  </div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<?php
+include "dbBroker.php";
+include "Model/Restaurant.php";
+
+if (isset($_POST['register'])) {
+    $name = trim($_POST['name']);
+    $password = trim($_POST['password']);
+    $confirmpassword = trim($_POST['confirmpassword']);
+  
+    $name = mysqli_real_escape_string($conn, $name);
+    $password = mysqli_real_escape_string($conn, $password);
+    $confirmpassword = mysqli_real_escape_string($conn, $confirmpassword);
+  
+    if ($password !== $confirmpassword) {
+        echo '<script>alert("Passwords do not match!")</script>';
+    } else {
+        $query = "SELECT * FROM restaurant WHERE name='$name'";
+        $res_e = mysqli_query($conn, $query);
+        if (mysqli_num_rows($res_e) > 0) {
+            echo '<script>alert("Name already exists!")</script>';
+        } else {
+            $data = array(
+                "name" => $name,
+                "password" => $password,
+            );
+            $restaurant = new Restaurant(null, $name, $password);
+            $restaurant->addRestaurant($data, $conn);
+            header("Location: index.php");
+            exit;
+        }
+    }
+}
+?>
+<html lang="en">
+  <head>
+    <title>Sign up</title>
+    <link rel="stylesheet" href="./CSS/register.css" /> 
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/quietflow@1.0.2/lib/quietflow.min.js"></script>
+  </head>
+  
+
+  <body>
+  <div class="container" id="Form">
+    <form name="form" action="#" method="POST" class="login-form">
+      <h1>Sign up</h1>
+      <div class="flex-input">
+        <label class="">Name</label>
+        <input type="text" size="30" id="name" placeholder="Name" name="name" required>
+      </div>
+      <div class="flex-input">
+        <label for="">Password</label>
+        <input type="password" size="30"  id="password"placeholder="Password"name="password"required >
+      </div>
+      <div class="flex-input">
+        <label for="">Confirm Password</label>
+        <input type="password" size="30" placeholder="Confirm Password"name="confirmpassword"required >
+      </div>
+      <div>
+        <input type="submit" name="register" value="Submit" id="submit">
+      </div>
+
+<div class="flex-container">
+    <div style="text-align: right;">
+        <input type="reset"id="reset">
+</div>
+</div>
+<p class="para-2">
+ Already have an account? <a href="form.php">Login here</a>
+   </form>
+</div>
+  </body>
+</html>
